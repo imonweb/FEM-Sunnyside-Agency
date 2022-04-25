89542edf00126df67b75a30d02a8377f8bd241fa
Imon <contact@imonweb.co.uk>
2022-04-25 21:13:53 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { fontawesome } from 'purge-fontawesome/fontawesome-svg-icons';
+
+library.add(fontawesome);
+ 
+dom.watch();
+ 
+
+// import './styles.scss';
+// import './css-partials/header.scss'
+
+import './main.scss';
+
